29c061f60a4e5cea54d694a1044fe564ab34fa5d
Felix Gertz <felix@mesosphere.io>
2015-06-17 16:11:38 +0200

    Test apps request

---
+++ src/test/apps.test.js
@@ -0,0 +1,59 @@
+var expect = require("chai").expect;
+var _ = require("underscore");
+
+var config = require("../js/config/config");
+var AppsActions = require("../js/actions/AppsActions");
+var AppsEvents = require("../js/events/AppsEvents");
+var AppsStore = require("../js/stores/AppsStore");
+
+var expectAsync = require("./helpers/expectAsync");
+var HttpServer = require("./helpers/HttpServer").HttpServer;
+
+var server = new HttpServer({
+  address: "localhost",
+  port: 8181
+});
+
+describe("Apps", function () {
+
+  beforeEach(function (done) {
+    this.server = server
+    .setup([{
+      id: "app-1"
+    }, {
+      id: "app-2"
+    }], 200)
+    .start(function () {
+      AppsStore.once(AppsEvents.CHANGE, done);
+      AppsActions.requestApps();
+    });
+    config.apiURL = "http://localhost:8181/";
+  });
+
+  afterEach(function (done) {
+    this.server.stop(done);
+  });
+
+  describe("on apps request", function () {
+
+    it("updates the AppsStore on success", function (done) {
+      AppsStore.once(AppsEvents.CHANGE, function () {
+        expectAsync(function () {
+          expect(AppsStore.apps).to.have.length(2);
+        }, done);
+      });
+
+      AppsActions.requestApps();
+    });
+
+    it("handles failure gracefully", function (done) {
+      this.server.setup({ message: "Guru Meditation" }, 404);
+
+      AppsStore.once(AppsEvents.REQUEST_APPS_ERROR, done);
+
+      AppsActions.requestApps();
+    });
+
+  });
+
+});
