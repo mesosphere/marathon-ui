08de4ac52fa2dc98a8b04af97e61624cdde400b9
Felix Gertz <dev@felixgertz.de>
2015-06-19 23:10:03 +0200

    AppList sorting by state change

--- src/js/components/AppListComponent.jsx
+++ src/js/components/AppListComponent.jsx
@@ -19,7 +19,9 @@ var AppListComponent = React.createClass({
   getInitialState: function () {
     return {
       apps: [],
-      fetchState: States.STATE_LOADING
+      fetchState: States.STATE_LOADING,
+      sortKey: null,
+      sortDescending: false
     };
   },
 
@@ -48,31 +50,43 @@ var AppListComponent = React.createClass({
     });
   },
 
-  sortCollectionBy: function (comparator) {
-    var collection = this.props.collection;
-    comparator =
-      collection.sortKey === comparator && !collection.sortReverse ?
-      "-" + comparator :
-      comparator;
-    collection.setComparator(comparator);
-    collection.sort();
+  sortBy: function (sortKey) {
+    var state = this.state;
+
+    this.setState({
+      sortKey: sortKey,
+      sortDescending: state.sortKey === sortKey && !state.sortDescending
+    });
   },
 
   getAppNodes: function () {
     var state = this.state;
+    var sortKey = state.sortKey;
+    var props = this.props;
 
     return lazy(state.apps)
+      .sortBy(function (app) {
+        return app[sortKey];
+      }, state.sortDescending)
       .map(function (app) {
         return (
-          <AppComponent key={app.id} model={app} router={this.props.router} />
+          <AppComponent key={app.id} model={app} router={props.router} />
         );
       })
       .value();
   },
 
+  getCaret: function (sortKey) {
+    if (sortKey === this.state.sortKey) {
+      return (
+        <span className="caret"></span>
+      );
+    }
+    return null;
+  },
+
   render: function () {
     var state = this.state;
-    var sortKey = this.props.collection.sortKey;
 
     var loadingClassSet = classNames({
       "hidden": state.fetchState !== States.STATE_LOADING
@@ -88,7 +102,7 @@ var AppListComponent = React.createClass({
 
     var headerClassSet = classNames({
       "clickable": true,
-      "dropup": this.props.collection.sortReverse
+      "dropup": state.sortDescending
     });
 
     var tableClassSet = classNames({
@@ -111,23 +125,23 @@ var AppListComponent = React.createClass({
         <thead>
           <tr>
             <th>
-              <span onClick={this.sortCollectionBy.bind(null, "id")} className={headerClassSet}>
-                ID {sortKey === "id" ? <span className="caret"></span> : null}
+              <span onClick={this.sortBy.bind(null, "id")} className={headerClassSet}>
+                ID {this.getCaret("id")}
               </span>
             </th>
             <th className="text-right">
-              <span onClick={this.sortCollectionBy.bind(null, "mem")} className={headerClassSet}>
-                {sortKey === "mem" ? <span className="caret"></span> : null} Memory (MB)
+              <span onClick={this.sortBy.bind(null, "mem")} className={headerClassSet}>
+                {this.getCaret("mem")} Memory (MB)
               </span>
             </th>
             <th className="text-right">
-              <span onClick={this.sortCollectionBy.bind(null, "cpus")} className={headerClassSet}>
-                {sortKey === "cpus" ? <span className="caret"></span> : null} CPUs
+              <span onClick={this.sortBy.bind(null, "cpus")} className={headerClassSet}>
+                {this.getCaret("cpus")} CPUs
               </span>
             </th>
             <th className="text-right">
-              <span onClick={this.sortCollectionBy.bind(null, "instances")} className={headerClassSet}>
-                {sortKey === "instances" ? <span className="caret"></span> : null} Tasks / Instances
+              <span onClick={this.sortBy.bind(null, "instances")} className={headerClassSet}>
+                {this.getCaret("instances")} Tasks / Instances
               </span>
             </th>
             <th className="text-right">
@@ -136,8 +150,8 @@ var AppListComponent = React.createClass({
               </span>
             </th>
             <th className="text-right">
-              <span onClick={this.sortCollectionBy.bind(null, "isDeploying")} className={headerClassSet}>
-                {sortKey === "isDeploying" ? <span className="caret"></span> : null} Status
+              <span onClick={this.sortBy.bind(null, "isDeploying")} className={headerClassSet}>
+                {this.getCaret("isDeploying")} Status
               </span>
             </th>
           </tr>
