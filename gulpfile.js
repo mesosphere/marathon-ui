df7049d42393de2e3dbb57c8e9559f5753ef06d8
Pierluigi Cau <pierlo@mesosphere.io>
2015-06-22 16:08:26 +0200

    Rename env variable to NODE_ENV

--- gulpfile.js
+++ gulpfile.js
@@ -54,7 +54,7 @@ var webpackConfig = {
 // Use webpack to compile jsx into js,
 gulp.task("webpack", function (callback) {
   // Extend options with source mapping
-  if (process.env.GULP_ENV === "development" &&
+  if (process.env.NODE_ENV === "development" &&
     !process.env.DISABLE_SOURCE_MAP ||
     process.env.DISABLE_SOURCE_MAP === "false") {
     webpackConfig.devtool = "source-map";
@@ -133,7 +133,7 @@ var tasks = [
   "index"
 ];
 
-if (process.env.GULP_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   tasks.push("minify-css", "minify-js");
 }
 gulp.task("default", tasks);
