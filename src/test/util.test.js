6f486621ac5cff7ca95d121573947095934db596
Pierluigi Cau <pierlo@mesosphere.io>
2015-07-09 09:25:15 +0200

    Add util.extend
    This utility wraps the ES6 Object.assign() method to enforce an
    immutable-like behaviour. The same could also be achieved with
    _.extend({}, ...sources) but we are moving away from that dependency in
    the near future.
    
    This fixes gh-1770

--- src/test/util.test.js
+++ src/test/util.test.js
@@ -38,4 +38,39 @@ describe("util", function () {
 
   });
 
+  describe("extend", function () {
+
+    it("returns a new object with merged properties", function () {
+      var objA = {hello: "world", foo: "bar"};
+      var objB = {hello: "there", baz: "foo"};
+      var expectedResult = {hello: "there", foo: "bar", baz: "foo"};
+      var result = util.extend(objA, objB);
+      expect(result).to.deep.equal(expectedResult);
+    });
+
+    it("returns a new object without modifying the source", function () {
+      var objA = {hello: "world", foo: "bar"};
+      var objB = {hello: "there", baz: "foo"};
+      var result = util.extend(objA, objB);
+      expect(objA).to.deep.equal({hello: "world", foo: "bar"});
+    });
+
+    it("accepts several sources", function () {
+      var objA = {hello: "world", foo: "bar"};
+      var objB = {hello: "there", baz: "foo"};
+      var objC = {id: null, flag: true};
+      var expectedResult = {
+        hello: "there",
+        foo: "bar",
+        baz: "foo",
+        id: null,
+        flag: true
+      };
+
+      var result = util.extend(objA, objB, objC);
+      expect(result).to.deep.equal(expectedResult);
+    });
+
+  });
+
 });
