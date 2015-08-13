6d37fcd13a7833a1edcfdaa160473e29eac9aad0
Felix Gertz <felix@mesosphere.io>
2015-08-13 11:08:18 +0200

    Config.dev.js needs rootUrl to be set

--- src/js/config/config.template.js
+++ src/js/config/config.template.js
@@ -1,6 +1,8 @@
 var configDev = {
   // Defines the Marathon API URL,
   // leave empty to use the same as the UI is served.
-  apiURL: ""
+  apiURL: "",
+  // If the UI is served through a proxied URL, this can be set here.
+  rootUrl: ""
 };
 module.exports = configDev;
