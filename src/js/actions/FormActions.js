2c11f3e55f5cfd1a14726f3274dfd166622d3f1e
Pierluigi Cau <pierlo@mesosphere.io>
2015-09-03 16:11:33 +0200

    Introduce FormActions
    A reusable set of actions to be consumed by a form store.

---
+++ src/js/actions/FormActions.js
@@ -0,0 +1,30 @@
+var AppDispatcher = require("../AppDispatcher");
+var FormsEvents = require("../events/FormEvents");
+
+var FormActions = {
+  insert: function (fieldId, value = null, index = null) {
+    AppDispatcher.dispatch({
+      actionType: FormsEvents.INSERT,
+      fieldId: fieldId,
+      value: value,
+      index: index
+    });
+  },
+  update: function (fieldId, value, index = null) {
+    AppDispatcher.dispatch({
+      actionType: FormsEvents.UPDATE,
+      fieldId: fieldId,
+      value: value,
+      index: index
+    });
+  },
+  delete: function (fieldId, index = null) {
+    AppDispatcher.dispatch({
+      actionType: FormsEvents.DELETE,
+      fieldId: fieldId,
+      index: index
+    });
+  }
+};
+
+module.exports = FormActions;
