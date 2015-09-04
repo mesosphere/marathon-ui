c3a20067b3ef8577e0ae834b8a49cc7d6b7533a5
Felix Gertz <felix@mesosphere.io>
2015-09-04 14:39:40 +0200

    Add tests for insert and update on AppFormStore

--- src/test/appForm.test.js
+++ src/test/appForm.test.js
@@ -9,26 +9,6 @@ describe("App Form", function () {
 
   describe("on field update", function () {
 
-    it("updates the AppFormStore app object", function (done) {
-      AppFormStore.once(FormEvents.CHANGE, function () {
-        expectAsync(function () {
-          expect(AppFormStore.app.id).to.equal("/app-1/transformed");
-        }, done);
-      });
-
-      FormActions.update("appId", "/app-1");
-    });
-
-    it("handles AppFormStore update errors", function (done) {
-      AppFormStore.once(FormEvents.CHANGE_ERROR, function (data) {
-        expectAsync(function () {
-          expect(data.fieldId).to.equal("nonexistentField");
-        }, done);
-      });
-
-      FormActions.update("nonexistentField", "");
-    });
-
     it("handles AppFormStore field validation errors", function (done) {
       AppFormStore.once(FormEvents.FIELD_VALIDATION_ERROR, function (data) {
         expectAsync(function () {
@@ -41,14 +21,138 @@ describe("App Form", function () {
       FormActions.update("appId", "", 1);
     });
 
-    it("handles indexed grouped fields update", function (done) {
-      AppFormStore.once(FormEvents.CHANGE, function () {
-          expect(AppFormStore.app.env.get("ENV_KEY")).to.equal("ENV_VALUE");
-        }, done);
+    describe("the model", function () {
+
+      after(function () {
+        AppFormStore.app = {};
+        AppFormStore.fields = {};
       });
 
-      FormActions.update("env", {key: "ENV_KEY", value: "ENV_VALUE"}, 0);
+      it("updates correctly", function (done) {
+        AppFormStore.once(FormEvents.CHANGE, function () {
+          expectAsync(function () {
+            expect(AppFormStore.app.id).to.equal("/app-1/transformed");
+          }, done);
+        });
+
+        FormActions.update("appId", "/app-1");
+      });
+
+      it("doesn't store unknown fields", function (done) {
+        AppFormStore.once(FormEvents.CHANGE_ERROR, function (data) {
+          expectAsync(function () {
+            expect(data.fieldId).to.equal("nonexistentField");
+          }, done);
+        });
+
+        FormActions.update("nonexistentField", "");
+      });
+
+      describe("the env field", function () {
+
+        it("inserts a key-value pair", function (done) {
+          AppFormStore.once(FormEvents.CHANGE, function () {
+            expectAsync(function () {
+              expect(AppFormStore.app.env.ENV_KEY_1).to.equal("ENV_VALUE_1");
+            }, done);
+          });
+
+          FormActions.insert("env", {key: "ENV_KEY_1", value: "ENV_VALUE_1"});
+        });
+
+        it("inserts another key-value pair", function (done) {
+          AppFormStore.once(FormEvents.CHANGE, function () {
+            expectAsync(function () {
+              expect(AppFormStore.app.env.ENV_KEY_1).to.equal("ENV_VALUE_1");
+              expect(AppFormStore.app.env.ENV_KEY_2).to.equal("ENV_VALUE_2");
+            }, done);
+          });
+
+          FormActions.insert("env", {key: "ENV_KEY_2", value: "ENV_VALUE_2"});
+        });
+
+        it("updates a key-value pair", function (done) {
+          AppFormStore.once(FormEvents.CHANGE, function () {
+            expectAsync(function () {
+              expect(AppFormStore.app.env.ENV_KEY_1).to.equal("ENV_VALUE_1A");
+            }, done);
+          });
+
+          FormActions.update("env",
+            {key: "ENV_KEY_1", value: "ENV_VALUE_1A"},
+            0
+          );
+        });
+
+      });
     });
+
+    describe("the form fields object", function () {
+
+      it("updates correctly", function (done) {
+        AppFormStore.once(FormEvents.CHANGE, function () {
+          expectAsync(function () {
+            expect(AppFormStore.fields.appId).to.equal("/app-1");
+          }, done);
+        });
+
+        FormActions.update("appId", "/app-1");
+      });
+
+      describe("the env field", function () {
+
+        it("inserts a key-value pair", function (done) {
+          AppFormStore.once(FormEvents.CHANGE, function () {
+            expectAsync(function () {
+              expect(AppFormStore.fields.env[0]).to.deep.equal({
+                key: "ENV_KEY_1",
+                value: "ENV_VALUE_1"
+              });
+            }, done);
+          });
+          FormActions.insert("env", {key: "ENV_KEY_1", value: "ENV_VALUE_1"});
+        });
+
+        it("updates a key-value pair at index", function (done) {
+          AppFormStore.once(FormEvents.CHANGE, function () {
+            expectAsync(function () {
+              expect(AppFormStore.fields.env[0]).to.deep.equal({
+                key: "ENV_KEY_2",
+                value: "ENV_VALUE_1"
+              });
+            }, done);
+          });
+          FormActions.update("env",
+            {key: "ENV_KEY_2", value: "ENV_VALUE_1"},
+            0
+          );
+        });
+
+        it("inserts a key-value pair at index", function (done) {
+
+          AppFormStore.once(FormEvents.CHANGE, function () {
+            expectAsync(function () {
+              expect(AppFormStore.fields.env.length).to.equal(2);
+              expect(AppFormStore.fields.env[0]).to.deep.equal({
+                key: "ENV_KEY_3",
+                value: "ENV_VALUE_3"
+              });
+              expect(AppFormStore.fields.env[1]).to.deep.equal({
+                key: "ENV_KEY_2",
+                value: "ENV_VALUE_1"
+              });
+            }, done);
+          });
+
+          FormActions.insert("env",
+            {key: "ENV_KEY_3", value: "ENV_VALUE_3"},
+            0
+          );
+
+        });
+      });
+    });
+
   });
 
 });
