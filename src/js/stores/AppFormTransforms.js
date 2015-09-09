6ed0a842ae4ca5d156babe0af5edf359efb622be
Pierluigi Cau <pierlo@mesosphere.io>
2015-09-09 11:39:24 +0200

    Disk values can be floats

--- src/js/stores/AppFormTransforms.js
+++ src/js/stores/AppFormTransforms.js
@@ -1,6 +1,6 @@
 const AppFormTransforms = {
   appId: (id) => id + "/transformed",
-  disk: (value) => parseInt(value),
+  disk: (value) => parseFloat(value),
   env: (rows) => {
     return rows.reduce((memo, row) => {
       memo[row.key] = row.value;
--- src/js/stores/AppFormValidators.js
+++ src/js/stores/AppFormValidators.js
@@ -3,7 +3,8 @@ var Util = require("../helpers/Util");
 const AppFormValidators = {
   appIdNotEmpty: (str) => !Util.isEmptyString(str),
   appIdNoWhitespaces: (str) => str.match(/ /g) == null,
-  disk: (value) => value != null && value.toString().match(/^[0-9]+$/),
+  disk: (value) =>
+    !Util.isEmptyString(value) && value.toString().match(/^[0-9]\.+$/),
   env: (obj) => !(Util.isEmptyString(obj.key) && !Util.isEmptyString(obj.value))
 };
 
