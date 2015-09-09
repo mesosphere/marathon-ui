917f7eda82ec459da47316032a58778754d18f64
Pierluigi Cau <pierlo@mesosphere.io>
2015-09-09 11:06:04 +0200

    Convert disk field to use new form flow

--- src/js/stores/AppFormValidators.js
+++ src/js/stores/AppFormValidators.js
@@ -3,11 +3,14 @@ var Util = require("../helpers/Util");
 const AppFormValidators = {
   appIdNotEmpty: (str) => !Util.isEmptyString(str),
   appIdNoWhitespaces: (str) => str.match(/ /g) == null,
-  cpus: (value) => value != null && value.toString().match(/^[0-9\.]+$/),
+  cpus: (value) =>
+    !Util.isEmptyString(value) && value.toString().match(/^[0-9\.]+$/),
+  disk: (value) =>
+    !Util.isEmptyString(value) && value.toString().match(/^[0-9\.]+$/),
   env: (obj) =>
     !(Util.isEmptyString(obj.key) && !Util.isEmptyString(obj.value)),
   mem: (value) =>
-    !Util.isEmptyString(value) && value.toString().match(/^[0-9\.]+$/)
+    !Util.isEmptyString(value) && value.toString().match(/^[0-9\.]+$/),
 };
 
 module.exports = AppFormValidators;
