c606a5407a0bd2d2be68070550a777d524408b25
Felix Gertz <felix@mesosphere.io>
2015-09-11 15:08:50 +0200

    Fix intendation and align it

--- src/js/stores/AppFormValidators.js
+++ src/js/stores/AppFormValidators.js
@@ -21,41 +21,62 @@ function isValidPath(value) {
 
 const AppFormValidators = {
   appIdNotEmpty: (str) => !Util.isEmptyString(str),
+
   appIdNoWhitespaces: (str) => str.match(/ /g) == null,
-  cpus: (value) =>
-    !Util.isEmptyString(value) && !!value.toString().match(/^[0-9\.]+$/),
-  disk: (value) =>
-    !Util.isEmptyString(value) && !!value.toString().match(/^[0-9\.]+$/),
-  constraints: (constraints) => Util.isEmptyString(constraints) || constraints
-    .split(",")
-    .map((constraint) => constraint.split(":").map((value) => value.trim()))
-    .every((p) => {
-      if (p.length < 2 || p.length > 3) {
-        return false;
-      }
-      return ValidConstraints.indexOf(p[1].toLowerCase()) !== -1;
-    }),
+
   containerVolumesContainerPathIsValid: (obj) => isValidPath(obj.containerPath),
+
   containerVolumesHostPathIsValid: (obj) => isValidPath(obj.hostPath),
+
   containerVolumesModeNotEmpty: (obj) => !Util.isEmptyString(obj.mode),
-  env: (obj) => obj.hasOwnProperty("key") && obj.hasOwnProperty("value") &&
+
+  constraints: (constraints) => Util.isEmptyString(constraints) ||
+    constraints
+      .split(",")
+      .map((constraint) => constraint.split(":").map((value) => value.trim()))
+      .every((p) => {
+        if (p.length < 2 || p.length > 3) {
+          return false;
+        }
+        return ValidConstraints.indexOf(p[1].toLowerCase()) !== -1;
+      }),
+
+  cpus: (value) => !Util.isEmptyString(value) &&
+    !!value.toString().match(/^[0-9\.]+$/),
+
+  disk: (value) => !Util.isEmptyString(value) &&
+    !!value.toString().match(/^[0-9\.]+$/),
+
+  dockerImageNoWhitespaces: (str) => str.match(/ /g) == null,
+
+  dockerParameters: (obj) =>
     !(Util.isEmptyString(obj.key) && !Util.isEmptyString(obj.value)),
+
+  dockerPortMappingsContainerPortIsValid: (obj) =>
+    isValidPort(obj.containerPort),
+
+  dockerPortMappingsHostPortIsValid: (obj) => isValidPort(obj.hostPort),
+
+  dockerPortMappingsServicePortIsValid: (obj) => isValidPort(obj.servicePort),
+
+  dockerPortMappingsProtocolNotEmpty: (obj) =>
+    !Util.isEmptyString(obj.protocol),
+
+  env: (obj) => obj.hasOwnProperty("key") &&
+    obj.hasOwnProperty("value") &&
+    !(Util.isEmptyString(obj.key) &&
+    !Util.isEmptyString(obj.value)),
+
   executor: (str) => Util.isString(str) &&
     (new RegExp("^(|\\/\\/cmd|\\/?[^\\/]+(\\/[^\\/]+)*)$"))
       .test(str),
-  dockerImageNoWhitespaces: (str) => str.match(/ /g) == null,
-  dockerParameters: (obj) =>
-    !(Util.isEmptyString(obj.key) && !Util.isEmptyString(obj.value)),
-  dockerPortMappingsContainerPortIsValid: (obj) =>
-    isValidPort(obj.containerPort),
-  dockerPortMappingsHostPortIsValid: (obj) => isValidPort(obj.hostPort),
-  dockerPortMappingsServicePortIsValid: (obj) => isValidPort(obj.servicePort),
-  dockerPortMappingsProtocolNotEmpty: (obj) =>
-    !Util.isEmptyString(obj.protocol),
-  instances: (value) =>
-    !Util.isEmptyString(value) && !!value.toString().match(/^[0-9]+$/),
-  mem: (value) =>
-    !Util.isEmptyString(value) && !!value.toString().match(/^[0-9\.]+$/),
+
+  instances: (value) => !Util.isEmptyString(value) &&
+    !!value.toString().match(/^[0-9]+$/),
+
+  mem: (value) => !Util.isEmptyString(value) &&
+    !!value.toString().match(/^[0-9\.]+$/),
+
   ports: (ports) => Util.isEmptyString(ports) ||
     ports.split(",")
       .every((port) => port.toString().trim().match(/^[0-9]+$/))
