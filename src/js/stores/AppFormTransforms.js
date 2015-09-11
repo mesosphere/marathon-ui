2151983aa85488388574cfd7f0eeacf0c134f447
Pierluigi Cau <pierlo@mesosphere.io>
2015-09-11 12:52:39 +0200

    Add transforms for container settings fields

--- src/js/stores/AppFormTransforms.js
+++ src/js/stores/AppFormTransforms.js
@@ -1,9 +1,32 @@
+var Util = require("../helpers/Util");
+
+function trimDuplicableRows(rows) {
+  var trimmedRows = rows.map((row) => {
+    return Object.keys(row).reduce((memo, key) => {
+      if (row[key] != null
+        && key !== "consecutiveKey"
+        && !Util.isEmptyString(row[key])) {
+        memo[key] = row[key];
+      }
+      return memo;
+    }, {});
+  }).filter((row) => Object.keys(row).length);
+
+  if (trimmedRows.length) {
+    return trimmedRows;
+  }
+  return null;
+}
+
 const AppFormTransforms = {
   cpus: (value) => parseFloat(value),
   disk: (value) => parseFloat(value),
   constraints: (constraints) => constraints
     .split(",")
     .map((constraint) => constraint.split(":").map((value) => value.trim())),
+  containerVolumes: (rows) => trimDuplicableRows(rows),
+  dockerParameters: (rows) => trimDuplicableRows(rows),
+  dockerPortMappings: (rows) => trimDuplicableRows(rows),
   env: (rows) => {
     return rows.reduce((memo, row) => {
       memo[row.key] = row.value;
