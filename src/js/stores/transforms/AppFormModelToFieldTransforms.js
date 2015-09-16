9296fdaee733bd4d0b8e5d252586ae537269669e
Felix Gertz <dev@felixgertz.de>
2015-09-16 14:41:24 +0200

    Better indentation if chaining

--- src/js/stores/transforms/AppFormModelToFieldTransforms.js
+++ src/js/stores/transforms/AppFormModelToFieldTransforms.js
@@ -1,34 +1,39 @@
 const AppFormModelToFieldTransforms = {
-  constraints: (constraints) => { return constraints
-    .map((constraint) => constraint.join(":"))
-    .join(", ");
+  constraints: (constraints) => {
+    return constraints
+      .map((constraint) => constraint.join(":"))
+      .join(", ");
   },
-  dockerPortMappings: (mappings) => { return mappings
-    .map((row, i) => {
-      row.consecutiveKey = i;
-      return row;
-    });
+  dockerPortMappings: (mappings) => {
+    return mappings
+      .map((row, i) => {
+        row.consecutiveKey = i;
+        return row;
+      });
   },
-  dockerParameters: (parameters) => { return parameters
-    .map((row, i) => {
-      row.consecutiveKey = i;
-      return row;
-    });
+  dockerParameters: (parameters) => {
+    return parameters
+      .map((row, i) => {
+        row.consecutiveKey = i;
+        return row;
+      });
   },
-  containerVolumes: (volumes) => { return volumes
-    .map((row, i) => {
-      row.consecutiveKey = i;
-      return row;
-    });
+  containerVolumes: (volumes) => {
+    return volumes
+      .map((row, i) => {
+        row.consecutiveKey = i;
+        return row;
+      });
   },
-  env: (rows) => { return Object.keys(rows)
-    .map((rowKey, i) => {
-      return {
-        key: rowKey,
-        value: rows[rowKey],
-        consecutiveKey: i
-      };
-    });
+  env: (rows) => {
+    return Object.keys(rows)
+      .map((rowKey, i) => {
+        return {
+          key: rowKey,
+          value: rows[rowKey],
+          consecutiveKey: i
+        };
+      });
   },
   ports: (ports) => ports
     .join(", "),
