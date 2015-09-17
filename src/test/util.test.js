1eda0fcb22063bbe71a1e373247c85ba22d510fb
Felix Gertz <felix@mesosphere.io>
2015-09-17 11:49:57 +0200

    Remove deprecated Util param function

--- src/test/util.test.js
+++ src/test/util.test.js
@@ -3,41 +3,6 @@ var Util = require("../js/helpers/Util");
 
 describe("Util", function () {
 
-  describe("param", function () {
-
-    it("returns a valid urlencoded query string", function () {
-      var obj = {
-        hello: "world",
-        foo: "bar"
-      };
-      expect(Util.param(obj)).to.equal("hello=world&foo=bar");
-    });
-
-    it("handles spaces and other entities correctly", function () {
-      var obj = {
-        q: "is:open is:issue label:gui"
-      };
-      expect(Util.param(obj))
-        .to.equal("q=is%3Aopen%20is%3Aissue%20label%3Agui");
-    });
-
-    it("handles bad input gracefully", function () {
-      var obj = {};
-      expect(Util.param(obj)).to.equal("");
-
-      obj = [1, 2, 3];
-      expect(Util.param(obj)).to.equal("0=1&1=2&2=3");
-
-      obj = null;
-      expect(Util.param(obj)).to.equal(null);
-
-      obj = "already=foo&also=bar";
-      expect(Util.param(obj)).to.equal("already=foo&also=bar");
-
-    });
-
-  });
-
   describe("extendObject", function () {
 
     it("returns a new object with merged properties", function () {
