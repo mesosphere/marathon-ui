55cf7316a56e497f3554a87b51644290fc201983
Felix Gertz <felix@mesosphere.io>
2015-09-24 14:53:36 +0200

    Introduce health checks row scheme

---
+++ src/js/stores/healthChecksRowScheme.js
@@ -0,0 +1,15 @@
+var HealthCheckProtocols = require("../constants/HealthCheckProtocols");
+
+const healthChecksRowScheme = {
+  protocol: HealthCheckProtocols.COMMAND,
+  command: null,
+  path: null,
+  portIndex: 0,
+  gracePeriodSeconds: 300,
+  intervalSeconds: 60,
+  timeoutSeconds: 20,
+  maxConsecutiveFailures: 3,
+  ignoreHttp1xx: false
+};
+
+module.exports = Object.freeze(healthChecksRowScheme);
