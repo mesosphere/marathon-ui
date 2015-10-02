facdba76056f56d9b344a110b02dd273db593e0e
Felix Gertz <felix@mesosphere.io>
2015-10-02 14:14:26 +0200

    Introduce forbidden state

--- src/js/constants/States.js
+++ src/js/constants/States.js
@@ -2,7 +2,8 @@ const States = {
   STATE_LOADING: 0,
   STATE_ERROR: 1,
   STATE_SUCCESS: 2,
-  STATE_UNAUTHORIZED: 3
+  STATE_UNAUTHORIZED: 3,
+  STATE_FORBIDDEN: 4
 };
 
 module.exports = Object.freeze(States);
