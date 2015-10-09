21c8aa05a08e126517272e855d3f79f3c0b57374
Orlando Hohmeier <orlando@mesosphere.io>
2015-10-09 13:34:59 +0200

    Add methods to round the memory value
    Impl. methods to round the memory and attach the correct unit, using JEDEC and IEC units.

--- src/js/components/MemoryFieldComponent.jsx
+++ src/js/components/MemoryFieldComponent.jsx
@@ -6,9 +6,20 @@ var MemoryFieldComponent = React.createClass({
     megabytes: React.PropTypes.number.isRequired
   },
   render() {
+    // For a documentation of the different unit prefixes please refer to:
+    // https://en.wikipedia.org/wiki/Template:Quantities_of_bytes
+    const units = ["MB", "GB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+    const factor = 1024;
     const megabytes = this.props.megabytes;
+    let value = 0;
+    let index = 0;
+    if (megabytes > 0) {
+      index = Math.floor(Math.log(megabytes) / Math.log(factor));
+      value = Math.round(megabytes / Math.pow(factor, index));
+    }
+    console.log(value, index);
     return (
-      <span title={`${megabytes}MB`}>{`${megabytes}MB`}</span>
+      <span title={`${megabytes}MB`}>{`${value}${units[index]}`}</span>
     );
   }
 });
