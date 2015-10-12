e2ea0be9b6c5ae1c2e84a68a5576118b0a5fbd64
Orlando Hohmeier <orlando@mesosphere.io>
2015-10-12 10:20:19 +0200

    Use var instead of const and let in function scopes for consistency

--- src/js/components/MemoryFieldComponent.jsx
+++ src/js/components/MemoryFieldComponent.jsx
@@ -8,11 +8,11 @@ var MemoryFieldComponent = React.createClass({
   render() {
     // For a documentation of the different unit prefixes please refer to:
     // https://en.wikipedia.org/wiki/Template:Quantities_of_bytes
-    const units = ["MB", "GB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
-    const factor = 1024;
-    const megabytes = this.props.megabytes;
-    let value = 0;
-    let index = 0;
+    var units = ["MB", "GB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+    var factor = 1024;
+    var megabytes = this.props.megabytes;
+    var value = 0;
+    var index = 0;
     if (megabytes > 0) {
       index = Math.floor(Math.log(megabytes) / Math.log(factor));
       value = Math.round(megabytes / Math.pow(factor, index));
