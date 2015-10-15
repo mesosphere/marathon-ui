072c54eb5d5d1502fd6032058cbb7c6bf4cfdd83
Orlando Hohmeier <orlando@mesosphere.io>
2015-10-15 12:07:13 +0200

    Remove the unused ViewHelper

--- src/test/viewHelper.test.js
+++ src/test/viewHelper.test.js
@@ -2,34 +2,6 @@ var expect = require("chai").expect;
 var ViewHelper = require("../js/helpers/ViewHelper");
 
 describe("ViewHelper", function () {
-
-  describe("convertMegabytesToString", function () {
-
-    it("appends the correct unit for megabytes", function () {
-      expect(ViewHelper.convertMegabytesToString(100)).to.equal("100MB");
-    });
-
-    it("appends the correct unit for gigabytes", function () {
-      expect(ViewHelper.convertMegabytesToString(1040)).to.equal("1GB");
-    });
-
-    it("appends the correct unit for tebibyte", function () {
-      expect(ViewHelper.convertMegabytesToString(1064960)).to.equal("1TiB");
-    });
-
-    it("handles 0 values correctly", function () {
-      expect(ViewHelper.convertMegabytesToString(0)).to.equal("0MB");
-    });
-
-    it("handles null values correctly", function () {
-      expect(ViewHelper.convertMegabytesToString(null)).to.equal("0MB");
-    });
-
-    it("handles undefined values correctly", function () {
-      expect(ViewHelper.convertMegabytesToString(undefined)).to.equal("0MB");
-    });
-
-  });
   describe("getRelativePath", function () {
     it("trims initial group from app names", function () {
       expect(ViewHelper.getRelativePath("/test/group/app", "/test/group"))
