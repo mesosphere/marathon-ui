b846836ed6b621074a7a4ba5496841c56d214c80
philipnrmn <philip@mesosphere.io>
2015-10-16 12:12:57 +0200

    Use ShallowUtils to get text from links

--- src/test/apps.test.js
+++ src/test/apps.test.js
@@ -3,7 +3,9 @@ var expect = require("chai").expect;
 var React = require("react/addons");
 var ReactContext = require("react/lib/ReactContext");
 var TestUtils = React.addons.TestUtils;
+
 var Util = require("../js/helpers/Util");
+var ShallowUtils = require("./helpers/ShallowUtils");
 
 /**
  * This *nasty* hack is needed because we want to prevent TooltipMixin from
@@ -1107,7 +1109,7 @@ describe("Breadcrumb Component", function () {
     var component = this.renderComponent();
     var rootLink = component.props.children[0].props.children;
     expect(rootLink.props.to).to.equal("apps");
-    expect(rootLink.props.children).to.equal("Applications");
+    expect(ShallowUtils.getText(rootLink)).to.equal("Applications");
   });
 
   it("renders group names correctly", function () {
@@ -1115,7 +1117,7 @@ describe("Breadcrumb Component", function () {
     var groupItems = component.props.children[1];
     var linkText = groupItems
       .filter((li) => !!li)
-      .map((li) => li.props.children.props.children);
+      .map((li) => ShallowUtils.getText(li.props.children));
 
     expect(linkText).to.deep.equal([
       "group-a", "group-b", "group-c"
@@ -1142,7 +1144,7 @@ describe("Breadcrumb Component", function () {
     var appLink = component.props.children[2].props.children;
     expect(appLink.props.to).to.equal("app");
     expect(appLink.props.params.appId).to.equal("%2Fgroup-a%2Fapp-1");
-    expect(appLink.props.children).to.equal("app-1");
+    expect(ShallowUtils.getText(appLink.props.children)).to.equal("app-1");
   });
 
   it("shows the task, if supplied", function () {
@@ -1152,7 +1154,7 @@ describe("Breadcrumb Component", function () {
     expect(taskLink.props.to).to.equal("appView");
     expect(taskLink.props.params.appId).to.equal("%2Fgroup-a%2Fapp-1");
     expect(taskLink.props.params.view).to.equal("task-1");
-    expect(taskLink.props.children).to.equal("task-1");
+    expect(ShallowUtils.getText(taskLink)).to.equal("task-1");
   });
 
 });
