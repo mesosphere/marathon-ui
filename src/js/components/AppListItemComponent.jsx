1a70168d789a37e879a43ba2e4ce0a98fb3c6a55
Orlando Hohmeier <orlando@mesosphere.io>
2015-10-16 16:31:52 +0200

    Remove unused class

--- src/js/components/AppListItemComponent.jsx
+++ src/js/components/AppListItemComponent.jsx
@@ -111,7 +111,7 @@ var AppListItemComponent = React.createClass({
           </span>
         </td>
         {this.getStatus()}
-        <td className="text-right running instances" colSpan={colSpan}>
+        <td className="text-right instances" colSpan={colSpan}>
           <span>
             {model.tasksRunning}
           </span> of {model.instances}
