29887ad6f9541624edb8888b9ce9474d363fcc76
Orlando Hohmeier <orlando@mesosphere.io>
2015-10-22 15:29:47 +0200

    Improve DOMUtil browser compatibility
    Add a getComputedStyle method which is aware of proprietary alternatives to the standardized window.getComputedStyle method.

--- src/js/helpers/DOMUtil.js
+++ src/js/helpers/DOMUtil.js
@@ -1,13 +1,19 @@
 var DOMUtil = {
+  getComputedStyle(node) {
+    if (typeof window.getComputedStyle === "undefined") {
+      return node.currentStyle;
+    }
+    return window.getComputedStyle(node);
+  },
   getInnerWidth(node) {
-    var computedStyle = window.getComputedStyle(node);
+    var computedStyle = this.getComputedStyle(node);
     var width = parseInt(node.clientWidth);
     width -= parseInt(computedStyle.paddingLeft);
     width -= parseInt(computedStyle.paddingRight);
     return width;
   },
   getOuterWidth(node) {
-    var computedStyle = window.getComputedStyle(node);
+    var computedStyle = this.getComputedStyle(node);
     var width = parseInt(node.clientWidth);
     width += parseInt(computedStyle.marginLeft);
     width += parseInt(computedStyle.marginRight);
