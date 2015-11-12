d46f878282ea2cc7fbad2a1debeaecffd8edce3c
Orlando Hohmeier <orlando@mesosphere.io>
2015-11-12 12:40:37 -0800

    Integrate reactjs table component
    Refactor the app list using the reactjs table component. This is the
    very first commit in a series to update the app list using reactjs
    components. It removes a lot of the current features (e.g. icons,
    groups, health bar, ...) and only displays the given app data and applies
    the selected filters.

--- src/js/components/AppListComponent.jsx
+++ src/js/components/AppListComponent.jsx
@@ -1,93 +1,29 @@
-var classNames = require("classnames");
-var lazy = require("lazy.js");
-var React = require("react/addons");
+import React from "react";
+import classNames from "classnames";
+import lazy from "lazy.js";
 
-var AppListViewTypes = require("../constants/AppListViewTypes");
-var AppStatus = require("../constants/AppStatus");
-var Messages = require("../constants/Messages");
-var States = require("../constants/States");
-var AppListItemComponent = require("./AppListItemComponent");
+import {Table} from "reactjs-components";
 
-var AppsStore = require("../stores/AppsStore");
-var AppsEvents = require("../events/AppsEvents");
+import States from "../constants/States";
+import AppsStore from "../stores/AppsStore";
+import AppsEvents from "../events/AppsEvents";
 
-function getGroupStatus(status, app) {
-  var appStatus = app.status;
+// @todo: discuss whether we should use *es7* static class members instead
+const displayName = "AppListComponent";
+const propTypes = {
+  currentGroup: React.PropTypes.string.isRequired,
+  filterLabels: React.PropTypes.array,
+  filterStatus: React.PropTypes.array,
+  filterText: React.PropTypes.string,
+  filterTypes: React.PropTypes.array
+};
+const defaultProps = {};
 
-  if (appStatus === AppStatus.DEPLOYING ||
-      appStatus === AppStatus.DELAYED ||
-      appStatus === AppStatus.WAITING) {
-    if (appStatus > status) {
-      return appStatus;
-    }
-  }
+export default class AppListComponent extends React.Component {
 
-  return status;
-}
+  constructor(props) {
+    super(props);
 
-function getGroupHealth(health, app) {
-  if (health == null) {
-    return app.health;
-  }
-
-  return health.map(healthState => {
-    var appHealthState =
-      app.health.find(appHealth => appHealth.state === healthState.state);
-
-    if (appHealthState != null) {
-      healthState.quantity += appHealthState.quantity;
-    }
-
-    return healthState;
-  });
-}
-
-function initGroupNode(groupId, app) {
-  return {
-    id: groupId,
-    instances: app.instances,
-    status: getGroupStatus(null, app),
-    health: getGroupHealth(null, app),
-    tasksRunning: app.tasksRunning,
-    totalCpus: app.totalCpus,
-    totalMem: app.totalMem,
-    isGroup: true
-  };
-}
-
-function updateGroupNode(group, app) {
-  group.instances += app.instances;
-  group.tasksRunning += app.tasksRunning;
-  group.totalCpus += app.totalCpus;
-  group.totalMem += app.totalMem;
-  group.health = getGroupHealth(group.health, app);
-  group.status = getGroupStatus(group.status, app);
-  return group;
-}
-
-var AppListComponent = React.createClass({
-  displayName: "AppListComponent",
-
-  contextTypes: {
-    router: React.PropTypes.func
-  },
-
-  propTypes: {
-    currentGroup: React.PropTypes.string.isRequired,
-    filterLabels: React.PropTypes.array,
-    filterStatus: React.PropTypes.array,
-    filterText: React.PropTypes.string,
-    filterTypes: React.PropTypes.array,
-    viewType: React.PropTypes.oneOf(Object.values(AppListViewTypes))
-  },
-
-  getDefaultProps: function () {
-    return {
-      viewType: AppListViewTypes.LIST
-    };
-  },
-
-  getInitialState: function () {
     var fetchState = States.STATE_LOADING;
     var apps = AppsStore.apps;
 
@@ -95,34 +31,38 @@ var AppListComponent = React.createClass({
       fetchState = States.STATE_SUCCESS;
     }
 
-    return {
+    // Set initial state
+    this.state = {
       apps: apps,
-      fetchState: fetchState,
-      sortKey: "id",
-      sortDescending: false
+      fetchState: fetchState
     };
-  },
 
-  componentWillMount: function () {
+    // Bind handler as they're not automatically bound by react
+    this.onAppsChange = this.onAppsChange.bind(this);
+    this.onAppsRequestError = this.onAppsRequestError.bind(this);
+  }
+
+  componentWillMount() {
     AppsStore.on(AppsEvents.CHANGE, this.onAppsChange);
     AppsStore.on(AppsEvents.REQUEST_APPS_ERROR, this.onAppsRequestError);
-  },
+  }
 
-  componentWillUnmount: function () {
+  componentWillUnmount() {
     AppsStore.removeListener(AppsEvents.CHANGE,
       this.onAppsChange);
     AppsStore.removeListener(AppsEvents.REQUEST_APPS_ERROR,
       this.onAppsRequestError);
-  },
+  }
 
-  onAppsChange: function () {
+  onAppsChange() {
+    console.log("onAppsChange");
     this.setState({
       apps: AppsStore.apps,
       fetchState: States.STATE_SUCCESS
     });
-  },
+  };
 
-  onAppsRequestError: function (message, statusCode) {
+  onAppsRequestError(message, statusCode) {
     var fetchState = States.STATE_ERROR;
 
     switch (statusCode) {
@@ -137,62 +77,119 @@ var AppListComponent = React.createClass({
     this.setState({
       fetchState: fetchState
     });
-  },
+  }
 
-  sortBy: function (sortKey) {
-    var state = this.state;
+  static getColGroup() {
+    return (
+      <colgroup>
+        <col className="icon-col"/>
+        <col className="name-col"/>
+        <col className="cpu-col"/>
+        <col className="ram-col"/>
+        <col className="status-col"/>
+        <col className="instances-col"/>
+        <col className="health-col"/>
+        <col className="actions-col"/>
+      </colgroup>
+    );
+  }
 
-    this.setState({
-      sortKey: sortKey,
-      sortDescending: state.sortKey === sortKey && !state.sortDescending
+  static getColumnHeading(prop, order, sortBy) {
+    let caretClassNames = classNames({
+      "caret": true,
+      "caret--asc": order === "asc",
+      "caret--desc": order === "desc",
+      "caret--visible": sortBy.prop === prop
     });
-  },
 
-  getGroupedNodes: function (appsSequence) {
-    var currentGroup = this.props.currentGroup;
+    let headingStrings = {
+      "icon": "Icon",
+      "name": "Name",
+      "cpu": "CPU",
+      "ram": "Memory",
+      "status": "Status",
+      "instances": "Instance",
+      "health": "Health",
+      "actions": "Actions"
+    };
 
-    return appsSequence
-      .filter(app => app.id.startsWith(currentGroup))
-      .reduce((memo, app) => {
-        let relativePath = app.id.substring(currentGroup.length);
-        let pathParts = relativePath.split("/");
-        let isGroup = pathParts.length > 1;
+    return (
+      <span>
+        {headingStrings[prop]}
+        <span className={caretClassNames}></span>
+      </span>
+    );
+  }
 
-        if (!isGroup) {
-          memo.push(app);
-        } else {
-          let groupId = currentGroup + pathParts[0];
-          let group = memo.find(item => {
-            return item.id === groupId;
-          });
+  static getColumns() {
+    return [
+      {
+        className: "icon-cell",
+        defaultContent: "I",
+        heading: this.getColumnHeading,
+        prop: "icon",
+        sortable: true
+      },
+      {
+        className: "name-cell",
+        heading: this.getColumnHeading,
+        prop: "id",
+        sortable: true
+      },
+      {
+        className: "cpu-cell",
+        heading: this.getColumnHeading,
+        prop: "cpu",
+        sortable: true
+      },
+      {
+        className: "ram-cell",
+        heading: this.getColumnHeading,
+        prop: "ram",
+        sortable: true
+      },
+      {
+        className: "status-cell",
+        heading: this.getColumnHeading,
+        prop: "status",
+        sortable: true
+      },
+      {
+        className: "instances-cell",
+        heading: this.getColumnHeading,
+        prop: "instances",
+        sortable: true
+      },
+      {
+        className: "health-cell",
+        heading: this.getColumnHeading,
+        prop: "health",
+        sortable: true
+      },
+      {
+        className: "actions-cell",
+        defaultContent: "...",
+        heading: this.getColumnHeading,
+        prop: "actions",
+        sortable: true
+      }
+    ];
+  }
 
-          if (group == null) {
-            group = initGroupNode(groupId, app);
-            memo.push(group);
-          } else {
-            updateGroupNode(group, app);
-          }
-        }
-        return memo;
-      }, []);
-  },
+  getApps() {
 
-  getAppNodes: function () {
-    var state = this.state;
-    var sortKey = state.sortKey;
     var props = this.props;
-
-    var appsSequence = lazy(state.apps);
+    var apps = this.state.apps;
 
     if (props.filterText != null && props.filterText !== "") {
-      appsSequence = appsSequence
+      apps = apps
         .filter(function (app) {
           return app.id.indexOf(props.filterText) !== -1;
         });
     }
 
     if (props.filterLabels != null && props.filterLabels.length > 0) {
-      appsSequence = appsSequence.filter(function (app) {
+      apps = apps.filter(function (app) {
         let labels = app.labels;
         if (labels == null || Object.keys(labels).length === 0) {
           return false;
@@ -206,7 +203,7 @@ var AppListComponent = React.createClass({
     }
 
     if (props.filterStatus != null && props.filterStatus.length > 0) {
-      appsSequence = appsSequence.filter(function (app) {
+      apps = apps.filter(function (app) {
         if (app.status == null) {
           return false;
         }
@@ -219,182 +216,40 @@ var AppListComponent = React.createClass({
     }
 
     if (props.filterTypes != null && props.filterTypes.length > 0) {
-      appsSequence = appsSequence.filter(function (app) {
+      apps = apps.filter(function (app) {
         return lazy(props.filterTypes).some(function (type) {
           return app.type === type;
         });
       });
     }
 
-    appsSequence
-      // Alphabetically presort
-      .sortBy((app) => {
-        return app.id;
-      }, state.sortDescending);
+    return apps;
+  }
 
-    let sortDirection = state.sortDescending ? 1 : -1;
-
-    return this.getGroupedNodes(appsSequence)
-      // Hoist groups to top of the app list and sort everything by sortKey
-      .sort((a, b) => {
-        if (a.isGroup && !b.isGroup) {
-          return -1;
-        } else if (b.isGroup && !a.isGroup) {
-          return 1;
-        } else {
-          return a[sortKey] > b[sortKey]
-            ? -1 * sortDirection
-            : 1 * sortDirection;
-        }
-      })
-      .map((app) => {
-        switch (props.viewType) {
-          case AppListViewTypes.LIST:
-            return (
-              <AppListItemComponent key={app.id}
-                model={app}
-                currentGroup={props.currentGroup} />
-            );
-          default:
-            return null;
-        }
-      });
-  },
-
-  getCaret: function (sortKey) {
-    if (sortKey === this.state.sortKey) {
-      return (
-        <span className="caret"></span>
-      );
-    }
-    return null;
-  },
-
-  render: function () {
+  render() {
     var state = this.state;
-    var appNodes = this.getAppNodes();
-
-    var pageIsLoading = state.fetchState === States.STATE_LOADING;
-    var pageHasApps = state.apps.length > 0;
-
-    var loadingClassSet = classNames({
-      "hidden": !pageIsLoading
-    });
-
-    var noAppsClassSet = classNames({
-      "hidden": pageIsLoading || pageHasApps ||
-        state.fetchState === States.STATE_UNAUTHORIZED ||
-        state.fetchState === States.STATE_FORBIDDEN
-    });
-
-    var noRunningAppsClassSet = classNames({
-      "hidden": !pageHasApps || appNodes.length > 0
-    });
-
-    var errorClassSet = classNames({
-      "hidden": state.fetchState !== States.STATE_ERROR
-    });
-
-    var unauthorizedClassSet = classNames({
-      "hidden": state.fetchState !== States.STATE_UNAUTHORIZED
-    });
-
-    var forbiddenClassSet = classNames({
-      "hidden": state.fetchState !== States.STATE_FORBIDDEN
-    });
-
-    var headerClassSet = classNames({
-      "clickable": true,
-      "dropup": !state.sortDescending
-    });
 
     var tableClassSet = classNames({
       "table table-fixed app-list": true,
-      "table-hover table-selectable":
-        state.apps.length !== 0 &&
-        state.fetchState !== States.STATE_LOADING
+      "flush-bottom": true,
+      "table-hover table-selectable": state.apps.length !== 0 &&
+      state.fetchState !== States.STATE_LOADING
     });
 
     return (
-      <table className={tableClassSet}>
-        <colgroup>
-          <col className="icon-col" />
-          <col className="name-col" />
-          <col className="cpu-col" />
-          <col className="ram-col" />
-          <col className="status-col" />
-          <col className="instances-col" />
-          <col className="health-col" />
-          <col className="actions-col" />
-        </colgroup>
-        <thead>
-          <tr>
-            <th className="text-left name-cell" colSpan="2">
-              <span onClick={this.sortBy.bind(null, "id")}
-                  className={headerClassSet}>
-                Name {this.getCaret("id")}
-              </span>
-            </th>
-            <th className="text-right cpu-cell">
-              <span onClick={this.sortBy.bind(null, "totalCpus")}
-                  className={headerClassSet}>
-                {this.getCaret("totalCpus")} CPU
-              </span>
-            </th>
-            <th className="text-right ram-cell">
-              <span onClick={this.sortBy.bind(null, "totalMem")}
-                    className={headerClassSet}>
-                {this.getCaret("totalMem")} Memory
-              </span>
-            </th>
-            <th className="status-cell">
-              <span onClick={this.sortBy.bind(null, "status")}
-                    className={headerClassSet}>
-                Status {this.getCaret("status")}
-              </span>
-            </th>
-            <th className="text-right instances-cell" colSpan="3">
-              <span onClick={this.sortBy.bind(null, "tasksRunning")}
-                  className={headerClassSet}>
-                {this.getCaret("tasksRunning")} Running Instances
-              </span>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className={loadingClassSet}>
-            <td className="text-center text-muted" colSpan="8">
-              Loading apps...
-            </td>
-          </tr>
-          <tr className={noAppsClassSet}>
-            <td className="text-center" colSpan="8">No running apps.</td>
-          </tr>
-          <tr className={noRunningAppsClassSet}>
-            <td className="text-center" colSpan="8">
-              No apps match your query.
-            </td>
-          </tr>
-          <tr className={errorClassSet}>
-            <td className="text-center text-danger" colSpan="8">
-              {`Error fetching apps. ${Messages.RETRY_REFRESH}`}
-            </td>
-          </tr>
-          <tr className={unauthorizedClassSet}>
-            <td className="text-center text-danger" colSpan="8">
-              {`Error fetching apps. ${Messages.UNAUTHORIZED}`}
-            </td>
-          </tr>
-          <tr className={forbiddenClassSet}>
-            <td className="text-center text-danger" colSpan="8">
-              {`Error fetching apps. ${Messages.FORBIDDEN}`}
-            </td>
-          </tr>
-          {appNodes}
-        </tbody>
-      </table>
+      <section>
+        <Table
+          className={tableClassSet}
+          colGroup={AppListComponent.getColGroup()}
+          columns={AppListComponent.getColumns()}
+          data={this.getApps()}
+          idAttribute="id"
+          transition={false}/>
+      </section>
     );
   }
-});
+}
 
-module.exports = AppListComponent;
+AppListComponent.displayName = displayName;
+AppListComponent.propTypes = propTypes;
+AppListComponent.defaultProps = defaultProps;
