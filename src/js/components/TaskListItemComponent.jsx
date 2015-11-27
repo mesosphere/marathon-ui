7ac6bd1716f4c7e2c4902197ce255f059a349a1d
Pierluigi Cau <pierlo@mesosphere.io>
2015-11-27 18:23:43 +0100

    Add support for service discovery endpoints
    Show the right endpoints if the service discovery info was supplied by
    the user.

--- src/js/components/TaskListItemComponent.jsx
+++ src/js/components/TaskListItemComponent.jsx
@@ -2,51 +2,9 @@ var classNames = require("classnames");
 var React = require("react/addons");
 var Moment = require("moment");
 
+var AppsStore = require("../stores/AppsStore");
 var HealthStatus = require("../constants/HealthStatus");
 var TaskStatus = require("../constants/TaskStatus");
-
-function buildHref(host, port) {
-  return "http://" + host + ":" + port;
-}
-
-function buildTaskAnchors(task) {
-  var taskAnchors;
-  var ports = task.ports;
-  var portsLength = ports.length;
-
-  if (portsLength > 1) {
-    // Linkify each port with the hostname. The port is the text of the
-    // anchor, but the href contains the hostname and port, a full link.
-    taskAnchors = (
-      <span className="text-muted">
-        {task.host}:[{ports.map(function (p, index) {
-          return (
-            <span key={p}>
-              <a className="text-muted" href={buildHref(task.host, p)}>
-                {p}
-              </a>
-              {index < portsLength - 1 ? ", " : ""}
-            </span>
-          );
-        })}]
-      </span>
-    );
-  } else if (portsLength === 1) {
-    // Linkify the hostname + port since there is only one port.
-    taskAnchors = (
-      <a className="text-muted" href={buildHref(task.host, ports[0])}>
-        {task.host}:{ports[0]}
-      </a>
-    );
-  } else {
-    // Ain't no ports; don't linkify.
-    taskAnchors = (
-      <span className="text-muted">{task.host}</span>
-    );
-  }
-
-  return taskAnchors;
-}
 
 var TaskListItemComponent = React.createClass({
   displayName: "TaskListItemComponent",
@@ -58,6 +16,110 @@ var TaskListItemComponent = React.createClass({
     onToggle: React.PropTypes.func.isRequired,
     task: React.PropTypes.object.isRequired,
     taskHealthMessage: React.PropTypes.string
+  },
+
+  getHostAndPorts: function () {
+    var task = this.props.task;
+    var ports = task.ports;
+
+    if (ports == null || ports.length === 0 ) {
+      return (<span className="text-muted">{task.host}</span>);
+    }
+
+    if (ports != null && ports.length === 1) {
+      return (
+        <a className="text-muted" href={`//${task.host}:${ports[0]}`}>
+          {`${task.host}:${ports[0]}`}
+        </a>
+      );
+    }
+
+    if (ports != null && ports.length > 1) {
+      let portNodes = ports.map(function (port, index) {
+        return (
+          <span key={port}>
+            <a className="text-muted" href={`//${task.host}:${port}`}>
+              {port}
+            </a>
+            {index < ports.length - 1 ? ", " : ""}
+          </span>
+        );
+      });
+
+      return (
+        <span className="text-muted">
+          {task.host}:[{portNodes}]
+        </span>
+      );
+    }
+  },
+
+  getEndpoints: function () {
+    var task = this.props.task;
+
+    if (task.ipAddresses == null) {
+      return this.getHostAndPorts();
+    }
+
+    return this.getServiceDiscoveryEndpoints();
+  },
+
+  getServiceDiscoveryEndpoints: function () {
+    var props = this.props;
+    var task = props.task;
+    var app = AppsStore.getCurrentApp(props.appId);
+
+    if (app.ipAddress != null &&
+        app.ipAddress.discovery != null &&
+        app.ipAddress.discovery.ports != null &&
+        task.ipAddresses != null &&
+        task.ipAddresses.length > 0) {
+
+      let serviceDiscoveryPorts = app.ipAddress.discovery.ports;
+
+      let endpoints = task.ipAddresses.map((address, i) => {
+        let ipAddress = address.ipAddress;
+        let trailingComma = i < task.ipAddresses.length - 1;
+
+        if (serviceDiscoveryPorts.length === 1) {
+          let port = serviceDiscoveryPorts[0].number;
+          return (
+            <span className="text-muted" key={`${ipAddress}:${port}`}>
+              <a className="text-muted"
+                  href={`//${ipAddress}:${port}`}>
+                {`${ipAddress}:${port}`}
+              </a>
+              {trailingComma ? ", " : ""}
+            </span>
+          );
+        }
+
+        let portNodes = serviceDiscoveryPorts.map((port, j) => {
+          return (
+            <span key={port.number}>
+              <a className="text-muted"
+                  href={`//${ipAddress}:${port.number}`}>
+                {port.number}
+              </a>
+              {j < serviceDiscoveryPorts.length - 1 ? ", " : ""}
+            </span>
+          );
+        });
+
+        return (
+          <span className="text-muted">
+            {address.ipAddress}:[{portNodes}]
+            {trailingComma ? ", " : ""}
+          </span>
+        );
+      });
+
+      return (
+        <span className="text-muted">
+          {endpoints}
+        </span>
+      );
+    }
   },
 
   handleClick: function (event) {
@@ -119,7 +181,7 @@ var TaskListItemComponent = React.createClass({
         <td>
             <a href={taskUri}>{taskId}</a>
           <br />
-          {buildTaskAnchors(task)}
+          {this.getEndpoints()}
         </td>
         <td className={healthClassSet} title={this.props.taskHealthMessage}>
           {this.props.taskHealthMessage}
