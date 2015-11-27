0d6715ba34b6c0c334d6e90032a4ac76a5349ff7
Philip Norman <philip@mesosphere.io>
2015-11-27 02:56:39 +0100

    Show breakdown in health bar tooltip

--- src/js/components/AppHealthComponent.jsx
+++ src/js/components/AppHealthComponent.jsx
@@ -2,6 +2,7 @@ var classNames = require("classnames");
 var React = require("react/addons");
 
 var TooltipMixin = require("../mixins/TooltipMixin");
+var AppHealthBreakdownComponent = require("./AppHealthBreakdownComponent");
 var Util = require("../helpers/Util");
 var AppStatus = require("../constants/AppStatus");
 var HealthStatus = require("../constants/HealthStatus");
@@ -19,6 +20,12 @@ var healthNameMap = {
   [HealthStatus.UNSCHEDULED]: "unscheduled"
 };
 
+var appHealthBreakdownFields = [
+  {label: "Healthy", key: "healthy", state: HealthStatus.HEALTHY},
+  {label: "Unhealthy", key: "unhealthy", state: HealthStatus.UNHEALTHY},
+  {label: "Unknown", key: "unknown", state: HealthStatus.UNKNOWN}
+];
+
 var AppHealthComponent = React.createClass({
   displayName: "AppHealthComponent",
 
@@ -28,8 +35,19 @@ var AppHealthComponent = React.createClass({
     model: React.PropTypes.object.isRequired
   },
 
+  getInitialState: function () {
+    return {
+      tipContent: this.getAppHealthBreakdown()
+    };
+  },
+
   shouldComponentUpdate: function (nextProps) {
-    return !Util.compareArrays(this.props.model.health, nextProps.model.health);
+    var shouldUpdate =
+      !Util.compareArrays(this.props.model.health, nextProps.model.health);
+    if (shouldUpdate) {
+      this.setState({tipContent: this.getAppHealthBreakdown()});
+    }
+    return shouldUpdate;
   },
 
   handleMouseOverHealthBar: function (ref) {
@@ -55,7 +73,7 @@ var AppHealthComponent = React.createClass({
           data-behavior="show-tip"
           data-tip-type-class="default"
           data-tip-place="top"
-          data-tip-content="deploying"
+          data-tip-content={this.state.tipContent}
           onMouseOver=
             {this.handleMouseOverHealthBar.bind(null, "loadingBar")}
           onMouseOut=
@@ -94,7 +112,7 @@ var AppHealthComponent = React.createClass({
           "data-behavior": "show-tip",
           "data-tip-type-class": "default",
           "data-tip-place": "top",
-          "data-tip-content": name,
+          "data-tip-content": this.state.tipContent,
           "onMouseOver": this.handleMouseOverHealthBar.bind(null, ref),
           "onMouseOut": this.handleMouseOutHealthBar.bind(null, ref)
         };
@@ -108,6 +126,15 @@ var AppHealthComponent = React.createClass({
           key={i} />
       );
     }.bind(this));
+  },
+
+  getAppHealthBreakdown: function () {
+    let component = (
+      <AppHealthBreakdownComponent
+        fields={appHealthBreakdownFields}
+        model={this.props.model} />
+    );
+    return React.renderToString(component);
   },
 
   render: function () {
