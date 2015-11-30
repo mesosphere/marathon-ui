07b2c7238a5ea7169c199d01ba6acbd7c35660a8
Orlando Hohmeier <orlando@mesosphere.io>
2015-11-30 15:52:47 +0100

    Adjust the file list styles and behaviour
    * Add methods to sort the task file list
    * Adjust the table heading
    * Add a download link

--- src/js/components/TaskFileListComponent.jsx
+++ src/js/components/TaskFileListComponent.jsx
@@ -1,7 +1,11 @@
-var React = require( "react/addons");
-var MesosActions = require( "../actions/MesosActions");
-var MesosEvents = require( "../events/MesosEvents");
-var MesosStore = require( "../stores/MesosStore");
+var React = require("react/addons");
+var classNames = require("classnames");
+var lazy = require("lazy.js");
+
+var Util = require("../helpers/Util");
+var MesosActions = require("../actions/MesosActions");
+var MesosEvents = require("../events/MesosEvents");
+var MesosStore = require("../stores/MesosStore");
 
 var TaskFileListComponent = React.createClass({
   displayName: "TaskFileListComponent",
@@ -42,39 +46,131 @@ var TaskFileListComponent = React.createClass({
     });
   },
 
+  sortBy: function (sortKey) {
+    var state = this.state;
+
+    this.setState({
+      sortKey: sortKey,
+      sortDescending: state.sortKey === sortKey && !state.sortDescending
+    });
+  },
+
+  getCaret: function (sortKey) {
+    if (sortKey === this.state.sortKey) {
+      return (
+        <span className="caret"></span>
+      );
+    }
+    return null;
+  },
+
   getFileNodes: function () {
-    var files = this.state.files;
+    var state = this.state;
+    var files = state.files;
+    var sortKey = state.sortKey;
     if (files != null) {
-      return files.map((file) => {
-        return (<tr key={file.path}>
-          <td>{file.gid}</td>
-          <td>{file.mode}</td>
-          <td>{file.mtime}</td>
-          <td>{file.size}</td>
-          <td>{file.uid}</td>
-          <td>
-            <a href={file.download}
-              target="_blank">
-              download
-            </a>
-          </td>
-        </tr>);
-      });
+      return lazy(files)
+        .sortBy((app) => {
+          return app[sortKey];
+        }, state.sortDescending)
+        .map((file) => {
+          let lastModifiedDate = new Date(file.mtime);
+          let lastModifiedIsoString = lastModifiedDate.toISOString();
+          let lastModifiedLocaleString = lastModifiedDate.toLocaleString();
+          return (
+            <tr key={file.path}>
+              <td>
+                <span>{file.name}</span>
+                <a className="btn btn-default"
+                  href={file.download}
+                  download={`${lastModifiedIsoString}-${file.name}`}>
+                  <i className="icon icon-mini file"/> Download
+                </a>
+              </td>
+              <td>
+                <span>{file.mode}</span>
+              </td>
+              <td>
+                <span>{file.nlink}</span>
+              </td>
+              <td>
+                <span>{file.uid}</span>
+              </td>
+              <td>
+                <span>{file.gid}</span>
+              </td>
+              <td className="text-right">
+                <span>
+                  {`${Util.filesize(file.size * Math.pow(1024, 2), 0)}`}
+                </span>
+              </td>
+              <td className="text-right">
+                <time dateTime={lastModifiedIsoString}
+                  title={lastModifiedIsoString}>
+                  {lastModifiedLocaleString}
+                </time>
+              </td>
+            </tr>
+          );
+        })
+        .value();
     }
   },
 
   render: function () {
+    var headerClassSet = classNames({
+      "clickable": true,
+      "dropup": !this.state.sortDescending
+    });
     return (
-      <table className="table table-fixed table-hover table-selectable">
+      <table className="table table-hover table-selectable task-file-list">
         <thead>
-        <th>gid</th>
-        <th>mode</th>
-        <th>mtime</th>
-        <th>size</th>
-        <th>uid</th>
-        <th>path</th>
+        <th>
+          <span onClick={this.sortBy.bind(null, "name")}
+            className={headerClassSet}>
+                ID {this.getCaret("name")}
+          </span>
+        </th>
+        <th>
+          <span onClick={this.sortBy.bind(null, "mode")}
+            className={headerClassSet}>
+                Permissions {this.getCaret("mode")}
+          </span>
+        </th>
+        <th>
+          <span onClick={this.sortBy.bind(null, "nlink")}
+            className={headerClassSet}>
+                Nlink {this.getCaret("nlink")}
+          </span>
+        </th>
+        <th>
+          <span onClick={this.sortBy.bind(null, "uid")}
+            className={headerClassSet}>
+                Uid {this.getCaret("uid")}
+          </span>
+        </th>
+        <th>
+          <span onClick={this.sortBy.bind(null, "gid")}
+            className={headerClassSet}>
+                Gid {this.getCaret("gid")}
+          </span>
+        </th>
+        <th className="text-right">
+          <span onClick={this.sortBy.bind(null, "size")}
+            className={headerClassSet}>
+                Size {this.getCaret("size")}
+          </span>
+        </th>
+        <th className="text-right">
+           <span onClick={this.sortBy.bind(null, "mtime")}
+             className={headerClassSet}>
+                Last Modified {this.getCaret("mtime")}
+          </span>
+        </th>
         </thead>
+        <tbody>
         {this.getFileNodes()}
+        </tbody>
       </table>
     );
   }
