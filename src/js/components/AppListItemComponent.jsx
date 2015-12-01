ab2894004402dcb2e74e4899ed94efeb13e73830
Pierluigi Cau <pierlo@mesosphere.io>
2015-12-01 15:01:24 +0100

    Get rid of .label class for badges

--- src/js/components/AppListItemComponent.jsx
+++ src/js/components/AppListItemComponent.jsx
@@ -103,7 +103,7 @@ var AppListItemComponent = React.createClass({
 
     let labelsWidth = 0;
     let numberOfVisibleLabels = 0;
-    let labelNodes = React.findDOMNode(refs.labels).querySelectorAll(".label");
+    let labelNodes = React.findDOMNode(refs.labels).querySelectorAll(".badge");
 
     // labelNodes is not an Array, but a NodeList
     [...labelNodes].forEach(label => {
