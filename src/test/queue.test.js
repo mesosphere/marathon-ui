6721d442bfdea34f3f47821b0c49b198e04ccc17
Pierluigi Cau <pierlo@mesosphere.io>
2015-12-22 14:05:05 +0100

    Rewrite queue tests with nock

--- src/test/queue.test.js
+++ src/test/queue.test.js
@@ -1,21 +1,19 @@
 var expect = require("chai").expect;
+var expectAsync = require("./helpers/expectAsync");
+var nock = require("nock");
 
 var config = require("../js/config/config");
 var QueueActions = require("../js/actions/QueueActions");
 var QueueEvents = require("../js/events/QueueEvents");
 var QueueStore = require("../js/stores/QueueStore");
 
-var expectAsync = require("./helpers/expectAsync");
-var HttpServer = require("./helpers/HttpServer").HttpServer;
-
-var server = new HttpServer(config.localTestserverURI);
+var server = config.localTestserverURI;
 config.apiURL = "http://" + server.address + ":" + server.port + "/";
 
 describe("Queue", function () {
 
-  beforeEach(function (done) {
-    this.server = server
-    .setup({
+  before(function (done) {
+    var nockResponse = {
       "queue": [
         {
           "app": {
@@ -28,39 +26,32 @@ describe("Queue", function () {
           }
         }
       ]
-    }, 200)
-    .start(function () {
-      QueueStore.once(QueueEvents.CHANGE, function () {
-        done();
-      });
-      QueueActions.requestQueue();
-    });
-  });
+    };
 
-  afterEach(function (done) {
-    this.server.stop(done);
+    nock(config.apiURL)
+      .get("/v2/queue")
+      .reply(200, nockResponse);
+
+    QueueStore.once(QueueEvents.CHANGE, done);
+    QueueActions.requestQueue();
   });
 
   describe("on queue request", function () {
 
-    it("updates the QueueStore on success", function (done) {
-      QueueStore.once(QueueEvents.CHANGE, function () {
-        expectAsync(function () {
-          expect(QueueStore.queue).to.have.length(1);
-          expect(QueueStore.queue[0].app.id)
-            .to.equal("/app-1");
-          expect(QueueStore.queue[0].delay.overdue)
-            .to.equal(false);
-        }, done);
-      });
-
-      QueueActions.requestQueue();
+    it("updates the QueueStore on success", function () {
+      expect(QueueStore.queue).to.have.length(1);
+      expect(QueueStore.queue[0].app.id)
+        .to.equal("/app-1");
+      expect(QueueStore.queue[0].delay.overdue)
+        .to.equal(false);
     });
 
     it("handles failure gracefully", function (done) {
-      this.server.setup({
-        message: "Guru Meditation"
-      }, 404);
+      nock(config.apiURL)
+        .get("/v2/queue")
+        .reply(404, {
+          message: "Guru Meditation"
+        });
 
       QueueStore.once(QueueEvents.REQUEST_ERROR,
           function (error) {
@@ -72,15 +63,8 @@ describe("Queue", function () {
       QueueActions.requestQueue();
     });
 
-    it("returns the right delay time for a delayed app", function (done) {
-      QueueStore.once(QueueEvents.CHANGE, function () {
-        expectAsync(function () {
-          let timeLeftSeconds = QueueStore.getDelayByAppId("/app-1");
-          expect(timeLeftSeconds).to.equal(784);
-        }, done);
-      });
-
-      QueueActions.requestQueue();
+    it("returns the right delay time for a delayed app", function () {
+      expect(QueueStore.getDelayByAppId("/app-1")).to.equal(784);
     });
 
   });
@@ -88,8 +72,10 @@ describe("Queue", function () {
   describe("on app delay reset", function () {
 
     it("respond sucessfully on matching app", function (done) {
-      // Payload is empty on a forcefully stoped deployment
-      this.server.setup(undefined, 204);
+      // Payload is empty on a forcefully stopped deployment
+      nock(config.apiURL)
+        .delete("/v2/queue//app-1/delay")
+        .reply(204, undefined);
 
       QueueStore.once(QueueEvents.RESET_DELAY, function (appId) {
         expectAsync(function () {
@@ -101,9 +87,11 @@ describe("Queue", function () {
     });
 
     it("handles failure gracefully", function (done) {
-      this.server.setup({
-        message: "Guru Meditation"
-      }, 404);
+      nock(config.apiURL)
+        .delete("/v2/queue//app-1/delay")
+        .reply(404, {
+          message: "Guru Meditation"
+        });
 
       QueueStore.once(QueueEvents.RESET_DELAY_ERROR, function (error, appId) {
         expectAsync(function () {
