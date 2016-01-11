9e92462208635ddca0b0376eac3c20ee63c72c04
Pierluigi Cau <pierlo@mesosphere.io>
2016-01-11 14:36:50 +0100

    Create component for inline dialogs
    These serve as inline notifications that replace the generic “error”
    rows in the app list.

---
+++ src/js/components/CenteredInlineDialogComponent.jsx
@@ -0,0 +1,33 @@
+var classNames = require("classnames");
+var React = require("react/addons");
+var Link = require("react-router").Link;
+
+
+var CenteredInlineDialogComponent = React.createClass({
+  displayName: "CenteredInlineDialogComponent",
+
+  propTypes: {
+    additionalClasses: React.PropTypes.string,
+    children: React.PropTypes.node,
+    message: React.PropTypes.string.isRequired,
+    title: React.PropTypes.string.isRequired
+  },
+
+  render: function () {
+    var props = this.props;
+
+    var classSet = classNames("inline-dialog", props.additionalClasses);
+
+    return (
+      <div className="centered-content">
+        <div className={classSet}>
+          <h3 className="h3">{props.title}</h3>
+          <p className="text-muted">{props.message}</p>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = CenteredInlineDialogComponent;
