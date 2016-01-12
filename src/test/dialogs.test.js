8138f0c1b0a621aa4618d398b3133a98652ac171
Orlando Hohmeier <orlando@mesosphere.io>
2016-01-12 10:38:37 +0100

    Add dialog component tests

--- src/test/dialogs.test.js
+++ src/test/dialogs.test.js
@@ -1,3 +1,6 @@
+var describeWithDOM = require("enzyme").describeWithDOM;
+var mount = require("enzyme").mount;
+var shallow = require("enzyme").shallow;
 var expect = require("chai").expect;
 var React = require("react/addons");
 var TestUtils = React.addons.TestUtils;
@@ -783,6 +786,235 @@ describe("Dialog store", function () {
 
         DialogActions.dismissDialog({id: id});
       });
+    });
+
+  });
+
+});
+
+describe("Dialog components", function () {
+
+  describe("alert", function () {
+    var AlertDialogComponent = require("../js/components/AlertDialogComponent");
+
+    var dialogData = {
+      actionButtonLabel: "Test Button Label",
+      message: "Test Message",
+      severity: DialogSeverity.DANGER,
+      title: "Test Title"
+    };
+
+    before(function () {
+      this.component = shallow(<AlertDialogComponent data={dialogData} />);
+    });
+
+    after(function () {
+      this.component = null;
+    });
+
+    it("renders correct title", function () {
+      var title = this.component
+        .find(".modal-header").text();
+      expect(title).to.equal("Test Title");
+    });
+
+    it("renders correct message", function () {
+      var message = this.component
+        .find(".modal-body").text();
+      expect(message).to.equal("Test Message");
+    });
+
+    it("renders correct action button label", function () {
+      var actionButtonLabel = this.component
+        .find(".btn-success").text();
+      expect(actionButtonLabel).to.equal("Test Button Label");
+    });
+
+    it("renders correct severity indicator", function () {
+      expect(this.component.find(".dialog").hasClass("danger")).to.equal(true);
+    });
+
+    it("handles action button clicks", function (done) {
+      this.component.setProps({
+        data:dialogData,
+        onAccept: function () {
+          done();
+        },
+        onDismiss: function () {
+          done(new Error("Dismiss handler shouldn't be called"));
+        }
+      });
+      this.component.find(".btn-success").simulate("click");
+    });
+
+  });
+
+  describe("confirm", function () {
+    var ConfirmDialoglComponent =
+      require("../js/components/ConfirmDialoglComponent");
+
+    var dialogData = {
+      actionButtonLabel: "Test Button Label",
+      message: "Test Message",
+      severity: DialogSeverity.DANGER,
+      title: "Test Title"
+    };
+
+    before(function () {
+      this.component = shallow(<ConfirmDialoglComponent data={dialogData} />);
+    });
+
+    after(function () {
+      this.component = null;
+    });
+
+    it("renders correct title", function () {
+      var title = this.component
+        .find(".modal-header").text();
+      expect(title).to.equal("Test Title");
+    });
+
+    it("renders correct message", function () {
+      var message = this.component
+        .find(".modal-body").text();
+      expect(message).to.equal("Test Message");
+    });
+
+    it("renders correct action button label", function () {
+      var actionButtonLabel = this.component
+        .find(".btn-success").text();
+      expect(actionButtonLabel).to.equal("Test Button Label");
+    });
+
+    it("renders correct severity indicator", function () {
+      expect(this.component.find(".dialog").hasClass("danger")).to.equal(true);
+    });
+
+    it("handles action button clicks", function (done) {
+      this.component.setProps({
+        data:dialogData,
+        onAccept: function () {
+          done();
+        },
+        onDismiss: function () {
+          done(new Error("Dismiss handler shouldn't be called"));
+        }
+      });
+      this.component.find(".btn-success").simulate("click");
+    });
+
+    it("handles dismiss button clicks", function (done) {
+      this.component.setProps({
+        data:dialogData,
+        onAccept: function () {
+          done(new Error("Accept handler shouldn't be called"));
+        },
+        onDismiss: function () {
+          done();
+        }
+      });
+      this.component.find(".btn-default").simulate("click");
+    });
+
+  });
+
+  describeWithDOM("prompt", function () {
+    var PromptDialogComponent =
+      require("../js/components/PromptDialogComponent");
+
+    var dialogData = {
+      actionButtonLabel: "Test Button Label",
+      inputProperties: {
+        defaultValue:10,
+        type: "number"
+      },
+      message: "Test Message",
+      severity: DialogSeverity.DANGER,
+      title: "Test Title"
+    };
+
+    before(function () {
+      this.component = mount(<PromptDialogComponent data={dialogData} />);
+    });
+
+    after(function () {
+      React.unmountComponentAtNode(this.component.instance().getDOMNode());
+    });
+
+    it("renders correct title", function () {
+      var title = this.component
+        .find(".modal-header").text();
+      expect(title).to.equal("Test Title");
+    });
+
+    it("renders correct message", function () {
+      var message = this.component
+        .find(".modal-body").find("label").text();
+      expect(message).to.equal("Test Message");
+    });
+
+    it("renders correct action button label", function () {
+      var actionButtonLabel = this.component
+        .find(".btn-success").text();
+      expect(actionButtonLabel).to.equal("Test Button Label");
+    });
+
+    it("renders correct severity indicator", function () {
+      expect(this.component.find(".dialog").hasClass("danger")).to.equal(true);
+    });
+
+    it("has correct input type", function () {
+      var inputType = this.component
+        .find(".modal-body").find("input").props().type;
+      expect(inputType).to.equal("number");
+    });
+
+    it("has correct input default value", function () {
+      var inputDefaultValue = this.component
+        .find(".modal-body").find("input").props().defaultValue;
+      expect(inputDefaultValue).to.equal(10);
+    });
+
+    it("handles action button clicks", function (done) {
+      this.component.setProps({
+        data:dialogData,
+        onAccept: function () {
+          done();
+        },
+        onDismiss: function () {
+          done(new Error("Dismiss handler shouldn't be called"));
+        }
+      });
+      this.component.find(".btn-success").simulate("click");
+    });
+
+    it("handles dismiss button clicks", function (done) {
+      this.component.setProps({
+        data:dialogData,
+        onAccept: function () {
+          done(new Error("Accept handler shouldn't be called"));
+        },
+        onDismiss: function () {
+          done();
+        }
+      });
+      this.component.find(".btn-default").simulate("click");
+    });
+
+    it("passes correct input value along", function (done) {
+      this.component.setProps({
+        data:dialogData,
+        onAccept: function (value) {
+          expectAsync(function () {
+            expect(value).to.equal("10");
+          }, done);
+        },
+        onDismiss: ()=> {
+          done(new Error("Dismiss handler shouldn't be called"));
+        }
+      });
+
+      this.component.find(".btn-success").simulate("click");
     });
 
   });
