a5ec2597080675cfa3420a6ca0da2c9b58c37e66
Pierluigi Cau <pierlo@mesosphere.io>
2016-01-27 15:47:42 +0100

    Refactor TooltipComponent to allow graceful mouseleave
    This ensures that the tooltip is displayed when the user moves the
    mouse over its contents.

--- src/js/components/TooltipComponent.jsx
+++ src/js/components/TooltipComponent.jsx
@@ -1,9 +1,15 @@
 import React from "react/addons";
 import classNames from "classnames";
+import TimerMixin from "react-timer-mixin";
+
 import PopoverComponent from "../components/PopoverComponent";
+
+var popOverDisplayTimer = null;
 
 var TooltipComponent = React.createClass({
   displayName: "TooltipComponent",
+
+  mixins: [TimerMixin],
 
   propTypes: {
     children: React.PropTypes.node.isRequired,
@@ -24,28 +30,37 @@ var TooltipComponent = React.createClass({
     };
   },
 
-  handleMouseOver: function () {
+  handleMouseEnter: function () {
+    this.clearTimeout(popOverDisplayTimer);
     this.setState({
       isPopoverVisible: true
     });
   },
 
-  handleMouseOut: function () {
-    this.setState({
-      isPopoverVisible: false
-    });
+  handleMouseLeave: function () {
+    popOverDisplayTimer = this.setTimeout(() => {
+      this.setState({
+        isPopoverVisible: false
+      });
+    }, 250);
   },
 
   render: function () {
     var props = this.props;
+    var message = React.cloneElement(props.message, {
+      onClick: e => e.stopPropagation()
+    });
 
     return (
-      <div onMouseOver={this.handleMouseOver}
-          onMouseOut={this.handleMouseOut}>
+      <div className="tooltip-container"
+          onMouseEnter={this.handleMouseEnter}
+          onMouseLeave={this.handleMouseLeave}>
         <PopoverComponent
             className={classNames("tooltip", props.className)}
+            onMouseEnter={this.handleMouseEnter}
+            onMouseLeave={this.handleMouseLeave}
             visible={!props.disabled && this.state.isPopoverVisible}>
-          {props.message}
+          {message}
         </PopoverComponent>
         {props.children}
       </div>
