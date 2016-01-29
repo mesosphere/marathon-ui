1fb49db83e2dc06a9aa5db7957575376916ab22a
Felix Gertz <dev@felixgertz.de>
2016-01-29 18:46:55 +0100

    Introduce PluginComponentStore for persistence

--- src/js/mixins/PluginMountMixin.jsx
+++ src/js/mixins/PluginMountMixin.jsx
@@ -1,34 +1,64 @@
 /* eslint-disable no-unused-vars */
 import React from "react/addons";
 /* eslint-enable no-unused-vars */
-import PluginDispatcher from "../plugin/PluginDispatcher";
+import PluginComponentEvents from "../events/PluginComponentEvents";
+import PluginComponentStore from "../stores/PluginComponentStore";
 
 import Util from "../helpers/Util";
+
+var mountComponents = function () {
+  if (typeof this.pluginPlaces !== "function") {
+    return;
+  }
+
+  var places = this.pluginPlaces();
+
+  places.forEach(place => {
+    this.pluginPlace[place.key] = [];
+
+    var components = PluginComponentStore.getComponents()
+      .filter(component => {
+        return component.placeId === place.id;
+      });
+
+    components.forEach(component => {
+      var Component = component.component;
+      var componentKey = `${Component.displayName}-${Util.getUniqueId()}`;
+      this.pluginPlace[place.key].push(<Component key={componentKey} />);
+    });
+
+    if (components.length !== 0) {
+      this.forceUpdate();
+    }
+  });
+};
 
 var PluginMountMixin = {
   componentWillMount: function () {
     this.pluginPlace = {};
 
-    if (typeof this.pluginPlaces !== "function") {
+    mountComponents.call(this);
+
+    PluginComponentStore.on(PluginComponentEvents.CHANGE, this.addComponent);
+  },
+
+  componentWillUnmount: function () {
+    PluginComponentStore.removeListener(PluginComponentEvents.CHANGE,
+      this.addComponent);
+  },
+
+  addComponent: function (component) {
+    var place =
+      this.pluginPlaces().find(place => place.id === component.placeId);
+
+    if (place == null) {
       return;
     }
 
-    var places = this.pluginPlaces();
-
-    places.forEach(place => {
-      this.pluginPlace[place.key] = [];
-
-      PluginDispatcher.register(event => {
-        if (event.eventType === "INJECT_COMPONENT") {
-          if (event.placeId === place.id) {
-            let Component = event.component;
-            let componentKey = `${Component.displayName}-${Util.getUniqueId()}`;
-            this.pluginPlace[place.key].push(<Component key={componentKey} />);
-            this.forceUpdate();
-          }
-        }
-      });
-    });
+    let Component = component.component;
+    let componentKey = `${Component.displayName}-${Util.getUniqueId()}`;
+    this.pluginPlace[place.key].push(<Component key={componentKey} />);
+    this.forceUpdate();
   },
 
   getPluginComponents: function (mountPoint) {
