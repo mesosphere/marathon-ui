2752f920aa42761abd8f64efb7c12e4b8a39e1a4
Felix Gertz <dev@felixgertz.de>
2016-01-29 12:30:10 +0100

    Introduce PluginEventProxy to expose specific events

---
+++ src/js/plugin/PluginEventProxy.js
@@ -0,0 +1,35 @@
+import PluginDispatcher from "./PluginDispatcher";
+
+import AppsEvents from "../events/AppsEvents";
+import AppsStore from "../stores/AppsStore";
+
+import Util from "../helpers/Util";
+
+var proxies = [];
+
+function registerProxy(proxy) {
+  proxies.push(function () {
+    proxy.store.on(proxy.storeEvent, () => {
+      PluginDispatcher.dispatch({
+        eventType: proxy.pluginEvent,
+        data: Util.deepCopy(proxy.store[proxy.storeGetter])
+      });
+    });
+  });
+}
+
+registerProxy({
+  pluginEvent: "PLUGIN_APPS_STORE_CHANGE",
+  store: AppsStore,
+  storeEvent: AppsEvents.CHANGE,
+  storeGetter: "apps"
+});
+
+var PluginEventProxy = {
+  init: function () {
+    proxies.forEach(proxy => proxy());
+  }
+};
+
+export default PluginEventProxy;
+
