2e7065534e7dc41be70d9e633300140930c8e760
Felix Gertz <dev@felixgertz.de>
2016-01-30 15:04:52 +0100

    Introduce read-only plugin actions contant file

--- src/js/plugin/external/MarathonUIPluginAPI.js
+++ src/js/plugin/external/MarathonUIPluginAPI.js
@@ -1,9 +1,11 @@
+import PluginActions from "./PluginActions";
 import PluginDispatcher from "./PluginDispatcher";
 import PluginEvents from "./PluginEvents";
 import PluginHelper from "./PluginHelper";
 import PluginMountPoints from "./PluginMountPoints";
 
 var MarathonUIPluginAPI = {
+  PluginActions: PluginActions,
   PluginDispatcher: PluginDispatcher,
   PluginEvents: PluginEvents,
   PluginMountPoints: PluginMountPoints,
