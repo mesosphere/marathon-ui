8a86054ad6d6f8e99c3c5b0a72cddebb78307d54
Felix Gertz <felix@mesosphere.io>
2016-02-01 16:18:32 +0100

    Refactor PluginMountMixin to PluginMountPointComponent

--- src/js/components/SidebarComponent.jsx
+++ src/js/components/SidebarComponent.jsx
@@ -10,16 +10,13 @@ import SidebarStatusFilterComponent
   from "../components/SidebarStatusFilterComponent";
 
 import QueryParamsMixin from "../mixins/QueryParamsMixin";
-import PluginMountMixin from "../mixins/PluginMountMixin";
+import PluginMountPointComponent from "../components/PluginMountPointComponent";
 import PluginMountPoints from "../plugin/external/PluginMountPoints";
 
 var SidebarComponent = React.createClass({
   displayName: "SidebarComponent",
 
-  mixins: [
-    QueryParamsMixin,
-    PluginMountMixin
-  ],
+  mixins: [QueryParamsMixin],
 
   contextTypes: {
     router: React.PropTypes.func
@@ -27,13 +24,6 @@ var SidebarComponent = React.createClass({
 
   propTypes: {
     groupId: React.PropTypes.string.isRequired
-  },
-
-  pluginPlaces: function () {
-    return [{
-      id: PluginMountPoints.SIDEBAR_BOTTOM,
-      key: "bottom"
-    }];
   },
 
   render: function () {
@@ -71,7 +61,7 @@ var SidebarComponent = React.createClass({
           {this.getClearLinkForFilter(FilterTypes.LABELS)}
         </div>
         <SidebarLabelsFilterComponent />
-        {this.getPluginComponents("bottom")}
+        <PluginMountPointComponent placeId={PluginMountPoints.SIDEBAR_BOTTOM} />
       </nav>
     );
   }
