23fcf71012492a931166afde719080c08220832e
Orlando Hohmeier <orlando@mesosphere.io>
2016-02-05 13:36:08 +0100

    Remove test/example plugin
    The test/example isn't compatible with the newest plugin interace
    addtions/changes. Please use the marathon-ui-example-plugin instead.

--- examplePlugin/src/js/main.js
+++
@@ -1,8 +0,0 @@
-import ExamplePluginComponent from "./components/ExamplePluginComponent";
-
-var {PluginHelper, PluginMountPoints} = global.MarathonUIPluginAPI;
-
-PluginHelper.registerMe("examplePlugin-0.0.1");
-
-PluginHelper.injectComponent(ExamplePluginComponent,
-  PluginMountPoints.SIDEBAR_BOTTOM);
