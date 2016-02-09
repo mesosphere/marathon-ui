e84f78b45b130b410dc072ec8e9f7ff3c317f85a
Philipp Hinrichsen <philipph@mesosphere.io>
2016-02-09 15:23:47 +0100

    Add test for Optional Volumes Component

---
+++ src/test/units/OptionalVolumesComponent.test.js
@@ -0,0 +1,66 @@
+import {expect} from "chai";
+import {render, shallow} from "enzyme";
+
+import React from "react/addons";
+import OptionalVolumesComponent from "../../js/components/OptionalVolumesComponent.jsx";
+
+describe("Optional Volumes Component", function () {
+  describe("(no volume)", () => {
+    var component = shallow(
+      <OptionalVolumesComponent errorIndices={{}}
+        fields={{"volumes": null}}
+        getErrorMessage={()=>{}}/>
+    );
+
+    it("should display a button", () => {
+      expect(component.find("button")).to.have.length(1);
+    });
+
+    it("should have a button with type button", () => {
+      expect(component.find("button").first().props().type).to.equal("button");
+    });
+
+    it("should have a button with the right text", () => {
+      expect(component.find("button").first().props().children)
+        .to.equal("Add a persistent local volume");
+    });
+
+    it("should have the right title", () => {
+      expect(component.find("h4").first().props().children)
+        .to.equal("Persistent Local Volumes");
+    })
+  });
+  describe("(one volume)", () => {
+    var component = render(
+      <OptionalVolumesComponent errorIndices={{}}
+        fields={{"volumes": [{
+          consecutiveKey: "11454950241300",
+          mode: null,
+          path: "",
+          size: "1024"
+        }]}}
+        getErrorMessage={()=>{}}/>
+    );
+
+    it("should have the right size id", () => {
+      expect(component.find("input").get(0).attribs.id)
+        .to.equal("volumes.size.0");
+    });
+
+    it("should have the right size value", () => {
+      expect(component.find("input").get(0).attribs.value)
+        .to.equal("1024");
+    });
+
+    it("should have the right path id", () => {
+      expect(component.find("input").get(1).attribs.id)
+        .to.equal("volumes.path.0");
+    });
+
+    it("should have the right path value", () => {
+      expect(component.find("input").get(1).attribs.value)
+        .to.equal("");
+    });
+  });
+
+});
