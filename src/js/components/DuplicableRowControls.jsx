0b87b534588dc603090b58b181a5163048697c67
Philipp Hinrichsen <philipph@mesosphere.io>
2016-03-03 11:00:49 +0100

    Change remove button in duplicapable rows
    The Button changed position with the add button and has an
    opacity of 0 if disabled.

--- src/js/components/DuplicableRowControls.jsx
+++ src/js/components/DuplicableRowControls.jsx
@@ -19,16 +19,16 @@ var DuplicableRowControls = React.createClass({
     return (
       <div className="controls">
         <button type="button"
-            className={removeButtonClassSet}
-            onClick={this.props.handleRemoveRow}>
-          <i className="icon ion-ios-minus-outline"/>
-          <i className="icon icon-hover ion-ios-minus"/>
-        </button>
-        <button type="button"
             className="btn btn-link add"
             onClick={this.props.handleAddRow}>
           <i className="icon ion-ios-plus-outline"/>
           <i className="icon icon-hover ion-ios-plus"/>
+        </button>
+        <button type="button"
+            className={removeButtonClassSet}
+            onClick={this.props.handleRemoveRow}>
+          <i className="icon ion-ios-minus-outline"/>
+          <i className="icon icon-hover ion-ios-minus"/>
         </button>
       </div>
     );
