ee52676ecfb2cd18347c6238ddafcf7a9b8b5e94
Pierluigi Cau <pierlo@mesosphere.io>
2016-03-08 14:34:15 +0100

    WUT

--- src/test/reporter/reporterSwitch.js
+++ src/test/reporter/reporterSwitch.js
@@ -21,8 +21,9 @@ var reporter;
 if (process.env.TEAMCITY_VERSION == null) {
   reporter = require("../../../node_modules/mocha/lib/reporters/spec");
 } else {
-  console.log(getFiles("./node_modules/mocha-teamcity-reporter"));
-  reporter = require("./node_modules/mocha-teamcity-reporter");
+  console.log("TEAMCITY REPORTER");
+  console.log(getFiles("../../../node_modules/mocha-teamcity-reporter"));
+  reporter = require("../../../node_modules/mocha-teamcity-reporter");
 }
 
 exports = module.exports = reporter;
