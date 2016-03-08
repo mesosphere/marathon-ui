dc8a0919787c0ff60d7543a1bea444a4578024b9
Pierluigi Cau <pierlo@mesosphere.io>
2016-03-08 14:31:38 +0100

    WUT

--- src/test/reporter/reporterSwitch.js
+++ src/test/reporter/reporterSwitch.js
@@ -1,12 +1,28 @@
+
 // This file is loaded by mocha directly and does not support any of
 // the shiny new ES6 features.
+
+function getFiles(dir, files_) {
+  var fs = require("fs");
+  files_ = files_ || [];
+  var files = fs.readdirSync(dir);
+  for (var i in files) {
+    var name = dir + "/" + files[i];
+    if (fs.statSync(name).isDirectory()){
+      getFiles(name, files_);
+    } else {
+      files_.push(name);
+    }
+  }
+  return files_;
+}
 
 var reporter;
 if (process.env.TEAMCITY_VERSION == null) {
   reporter = require("../../../node_modules/mocha/lib/reporters/spec");
 } else {
-  console.log("Using TeamCity reporter");
-  reporter = require("../../../node_modules/mocha-teamcity-reporter");
+  console.log(getFiles("./node_modules/mocha-teamcity-reporter"));
+  reporter = require("./node_modules/mocha-teamcity-reporter");
 }
-console.log("reporter", reporter);
+
 exports = module.exports = reporter;
