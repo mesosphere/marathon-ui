e559850ae56499bda7bf86c6fec824193b2af4b5
Felix Gertz <dev@felixgertz.de>
2016-03-10 11:54:43 +0100

    Expose form actions to plugins

--- src/js/plugin/shared/PluginActions.js
+++ src/js/plugin/shared/PluginActions.js
@@ -1,7 +1,10 @@
 import Util from "../../helpers/Util";
 
 const PluginActions = {
-  "DIALOG_ALERT": "DIALOG_ALERT"
+  "DIALOG_ALERT": "DIALOG_ALERT",
+  "FORM_INSERT": "FORM_INSERT",
+  "FORM_UPDATE": "FORM_UPDATE",
+  "FORM_DELETE": "FORM_DELETE"
 };
 
 export default Util.fixObject(PluginActions, "PLUGIN_ACTION_");
